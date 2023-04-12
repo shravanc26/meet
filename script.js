74f1b83c03c3e9c44b6e20623c2e4bed7b88c4bb
shravanc26 <111572131+shravanc26@users.noreply.github.com>
2023-04-12 22:45:43 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+var a=prompt("Enter word which you like the most by me");
+  	if(a== "madarchod" )
+  	{
+  	    window.location.href="invitation.html";
+  	}
+  	else
+  	{
+  		alert("invalid details");
+  		window.location.href="index.html";
+  	}
+  	
